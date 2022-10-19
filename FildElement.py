b78ce12182952ec4a37c2702a0d124a9bd1f34a4
danlylacov <99447254+danlylacov@users.noreply.github.com>
2022-10-19 21:19:52 +0300

    Update FildElement.py

--- FildElement.py
+++ FildElement.py
@@ -1,63 +1,78 @@
+
 class FieldElement:
 
     def __init__(self, num, prime):
         if num >= prime or num < 0:
-            error = 'Num {} not in field range 0 to {}'.format(num, prime - 1)
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
             raise ValueError(error)
-        self.num = num  # 'эллемент поля
-        self.prime = prime  # размерность поля
+        self.num = num
+        self.prime = prime
 
-    def __repr__(self):  # вывод элемента класса
+    def __repr__(self):
         return 'FieldElement_{}({})'.format(self.prime, self.num)
 
-    def __eq__(self, other):  # проверка равенства двух объектов класса
+    def __eq__(self, other):
         if other is None:
             return False
         return self.num == other.num and self.prime == other.prime
 
-    def __ne__(self, other):  # проверка неравенства
-        if other is None:
-            return False
-        return self.num != other.num or self.prime != other.prime
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
 
-    def __add__(self, other):  # +
+    def __add__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in diffrent Fields')
+            raise TypeError('Cannot add two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
         num = (self.num + other.num) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
 
-    def __sub__(self, other):  # -
+    def __sub__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in diffrent Fields')
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
         num = (self.num - other.num) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
 
-    def __mul__(self, other):  # *
+    def __mul__(self, other):
         if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in diffrent Fields')
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
         num = (self.num * other.num) % self.prime
+        # We return an element of the same class
         return self.__class__(num, self.prime)
 
-    def __pow__(self, exponent):  # возведение в степень (**)
-        num = (self.num ** exponent) % self.prime
-        return self.__class__(num, self.prime)
-
-    def __truediv__(self, other):  # деление (/)
-        if self.prime != other.prime:
-            raise TypeError('Cannot add two numbers in diffrent Fields')
-        num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
-        return self.__class__(num, self.prime)
-
-    def __pow__(self, exponent):  # переопределение операции возведения в степень, для возведения в отриц. степень
-        n = exponent
-        while n < 0:
-            n += self.prime - 1
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
         num = pow(self.num, n, self.prime)
         return self.__class__(num, self.prime)
 
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        # use fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
+        return self.__class__(num=num, prime=self.prime)
 
 
-class Point:
+
+class Point: # x, y - координаты точки, а, б - задаём график
     # y**2 = x**2 + ax + b
     def __init__(self, x, y, a, b): # инициализация
         self.a = a
@@ -66,9 +81,8 @@ class Point:
         self.y = y
         if self.x is None and  self.y is None:
             return
-        if self.y**2 != self.x**3 + a*x + b: # проверка на нахождение точки на заданной прямой
-            raise ValueError('({}, {}) is not on curve'.format(x,y))
-
+        if self.y ** 2 != self.x ** 3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
     def __repr__(self): # вывод экземпляра класса
         return 'Point({},{},{},{})'.format(self.x,self.y, self.a, self.b)
 
@@ -98,16 +112,87 @@ class Point:
             y_3 = s*(self.x - x_3) - self.y
             return self.__class__(x_3, y_3, self.a, self.b)
 
-        if self == other: # cложение, если одна точка напротив другой
+        if self == other:
             s = (3 * self.x**2 + self.a) / (2 * self.y)
-            x = s**2 - 2 * self.x
+            x = s ** 2 - 2 * self.x
             y = s * (self.x - x) - self.y
             return self.__class__(x, y, self.a, self.b)
 
         if self == other and self.y == 0 * self.x: #  когда касательная проведена по вертикали
             return self.__class__(None, None, self.a, self.b)
 
-print(Point(2, 5,5,7)+(Point(-1,-1, 5, 7)))
+    def __rmul__(self, coefficient): # умножение на коэффициент
+        coef = coefficient
+        current = self  # <1>
+        result = self.__class__(None, None, self.a, self.b)  # <2>
+        while coef:
+            if coef & 1:  # <3>
+                result += current
+            current += current  # <4>
+            coef >>= 1  # <5>
+        return result
+
+A = 0
+B = 7
+P = 2**256 - 2**32 - 977 # размерность поля
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+
+
+class S256Field(FieldElement):
+
+    def __init__(self, num, prime=None):
+        super().__init__(num=num, prime=P)
+
+    def __repr__(self):
+        return '{:x}'.format(self.num).zfill(64)
+
+ # для уравнения кривой
+
+
+
+
+class S256Point(Point):
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
+        if type(x) == int:
+            super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
+        else:
+            super().__init__(x=x, y=y, a=a, b=b)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'S256Point(infinity)'
+        else:
+            return 'S256Point({}, {})'.format(self.x, self.y)
+
+    def __rmul__(self, coefficient):
+        coef = coefficient % N  # <1>
+        return super().__rmul__(coef)
+
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N-2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+        return total.x.num == sig.r
+
+
+
+
+# класс верификации подписи
+class Signature:
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+    def __repr__(self):
+        return 'Signature ({:x}, {:x})'.format(self.r, self.s)
+
+
+    
+
+
 
 
 
