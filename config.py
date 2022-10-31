533b7e4a9793a212e4c89fd653682978f95759b0
danlylacov <99447254+danlylacov@users.noreply.github.com>
2022-10-31 04:22:32 +0300

    Create config.py
    файл с глобальными переменными

---
+++ config.py
@@ -0,0 +1,7 @@
+A = 0
+B = 7
+P = 2 ** 256 - 2 ** 32 - 977  # размерность поля
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+G_x = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798 #
+G_y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8 # координаты G
