6c9a5114fde1894466c998bfacaf2482157ffe8f
danlylacov <99447254+danlylacov@users.noreply.github.com>
2022-11-01 16:17:58 +0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,71 @@
+
+from helper import *
+
+
+class Script:
+
+    def __init__(self, cmds = None):
+        if cmds is None:
+            self.cmds = []
+        else:
+            self.cmds = cmds
+
+    @classmethod
+    def parse(cls, s):
+        length = read_varint(s)
+        cmds = []
+        count = 0
+        while count < length:
+            current = s.read(1)
+            count+=1
+            current_byte = current[0]
+            if current_byte >= 1 and current_byte <= 75:
+                n = current_byte
+                cmds.append(s.read(n))
+                count += data_length + 1
+            elif current_byte == 76:
+                data_length = little_endian_to_int(s.read(1))
+                cmds.append(s.read(data_length))
+                count += data_length + 1
+            elif current_byte == 77:
+                data_length = little_endian_to_int(s.read(2))
+                cmds.append(s.read(data_length))
+                count += data_length + 2
+            else:
+                op_code = current_byte
+                cmds.append(op_code)
+        if count != length:
+            raise SyntaxError('Parsing script failed!')
+        return cls(cmds)
+
+    def raw_serialize(self):
+        result = b''
+        for cmd in self.cmds:
+            if type(cmd) == int:
+                result += int_to_little_endian(cmd, 1)
+            else:
+                length = len(cmd)
+            if length < 75:
+                result += int_to_little_endian(length, 1)
+            if length > 75 and length < 0x100:
+                result += int_to_little_endian(76, 1)
+                result += int_to_little_endian(length, 1)
+            if length >= 0x100 and length <= 520:
+                result += int_to_little_endian(77, 1)
+                result += int_to_little_endian(length, 2)
+            else:
+                raise ValueError('too long an cmd')
+                result += cmd
+        return result
+
+    def serialize(self):
+        result = self.raw_serialize()
+        total = len(result)
+        return encode_varint(total) + result
+
+
+
+
+
+
+
