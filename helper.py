9aae4b588c4f70084058e539ba9e74bacf86f24c
danlylacov <99447254+danlylacov@users.noreply.github.com>
2022-11-15 16:30:11 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -58,4 +58,29 @@ def little_endian_to_int(b): # байты в исх порядке -> целое
 def int_to_little_endian(n, length): # целое число -> байты в обратном порядке
     return n.to_bytes(length, 'big')
 
+def read_varint(s): # вариант в закод. виде -> возвр int
+    i = s.read(1)[0]
+    if i == 0xfd:
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        return little_endian_to_int(s.read(8))
+    else:
+        return i
+
+def encode_varint(i): # int -> вариант в байтовом представлении(для количества транзакций в классе Tx)
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return  b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
+
+def p2pkh_script(h160):# хеш-код -> сценарий ScriptPubKey
+    return Script([0x76, 0xa9, h160, 0x88, 0xac])
 
